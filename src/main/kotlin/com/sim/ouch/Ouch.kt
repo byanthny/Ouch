5260f5f6c81f42dc6ec88daff9386b26b19cb2db
JonoAugustine <jaugust8@jhu.edu>
2019-03-29 19:57:22 -0400

    ?

--- src/main/kotlin/com/sim/ouch/Ouch.kt
+++ src/main/kotlin/com/sim/ouch/Ouch.kt
@@ -1,10 +1,18 @@
 package com.sim.ouch
 
-interface Entity {
-
+/** The essence of... */
+interface Quidity {
+    val id: String
 }
 
+/** The simulation. */
 interface Existence {
+    enum class Status { WET, DRY }
     val name: String
     val capacity: Long
+}
+
+/** That which possess the [Existence]. */
+interface Simulator {
+    val name: String
 }
---
+++ src/main/kotlin/com/sim/ouch/Utilities.kt
@@ -0,0 +1,3 @@
+package com.sim.ouch
+
+data class IDGenerator(val )
