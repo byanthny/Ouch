7eff2960a781383f768c28b38682ca743027aeb0
Anthony D <adas22@jhu.edu>
2019-03-30 00:37:23 -0400

    Fixed transitions and layout

--- front/js/brains.js
+++ front/js/brains.js
@@ -1,11 +1,11 @@
 function switchState() {
-    document.getElementByClassName("submit-button").classList.toggle("hidden");
-    document.getElementByClassName("search").classList.toggle("hidden");
-    document.getElementByClassName("user-input").classList.toggle("login");
-    document.getElementByClassName("header").classList.toggle("login");
+    document.getElementById("submit-button").classList.toggle("hidden");
+    document.getElementById("search").classList.toggle("opacity");
+    document.getElementById("user-input").classList.toggle("login");
+    document.getElementById("header").classList.toggle("login");
 }
 
 function switchDark() {
-    document.getElementByClassName("ouch").classList.toggle("dark");
-    document.getElementByClassName("header").classList.toggle("dark");
+    document.getElementById("ouch").classList.toggle("dark");
+    document.getElementById("header").classList.toggle("dark");
 }
