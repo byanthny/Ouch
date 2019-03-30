3606c1955d5e99391d0c43dba9ea155ba272a618
Anthony D <adas22@jhu.edu>
2019-03-30 15:46:36 -0400

    added closing of popup

--- docs/js/brains.js
+++ docs/js/brains.js
@@ -1,5 +1,4 @@
 function switchState() {
-    document.getElementById("submit-button").classList.toggle("hidden");
     document.getElementById("search").classList.toggle("opacity");
     document.getElementById("user-input").classList.toggle("login");
     document.getElementById("header").classList.toggle("login");
@@ -8,4 +7,13 @@ function switchState() {
 function switchDark() {
     document.getElementById("ouch").classList.toggle("dark");
     document.getElementById("header").classList.toggle("dark");
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    document.getElementById("help").classList.toggle("opacity");
+    setTimeout(function (){
+        document.getElementById("help").classList.toggle("hidden");
+    }, 1000);
 }
