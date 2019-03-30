eecd71b5c250c3aa81e655c25389d55277fbb439
Anthony D <adas22@jhu.edu>
2019-03-30 15:50:03 -0400

    added closing of popup

--- docs/js/brains.js
+++ docs/js/brains.js
@@ -1,4 +1,5 @@
 function switchState() {
+    document.getElementById("submit-button").classList.toggle("hidden");
     document.getElementById("search").classList.toggle("opacity");
     document.getElementById("user-input").classList.toggle("login");
     document.getElementById("header").classList.toggle("login");
