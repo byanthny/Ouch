c37be6b46c2b1a0d66a812b7a0b0832fb152f29c
Anthony D <adas22@jhu.edu>
2019-03-30 21:29:07 -0400

    websocket made connection

--- frontend/js/socket.js
+++ frontend/js/socket.js
@@ -3,25 +3,27 @@ var id = 'id';
 
 var url = 'wss://sim-ouch.herokuapp.com/ws?name=user&exID=id';
 
-var connection;
-
 //On button click update connected and stuff
 document.getElementById("submit-button").onclick = function() {
     usr = document.getElementById('user-input').value;
     id = document.getElementById('exist-input').value;
     url = 'wss://sim-ouch.herokuapp.com/ws?name='+usr+'&exID='+id;
-
-    connection = new WebSocket(url);
+    play();
     //alert(url);
 };
-connection.onopen = () => {
-    //do stuff and switch
-    switchState();
-    id = document.getElementById('user-input').reset();
-}
 
-connection.onerror = error => {
-    //create error message
-    alert(`WebSocket error: ${error}`);
+function play() {
+
+    var connection = new WebSocket(url);
+
+    connection.onopen = () => {
+        switchState();
+        document.getElementById('user-input').reset();
+    }
+
+    connection.onerror = error => {
+        //create error message
+        alert(`WebSocket error: ${error}`);
+    }
 }
 
