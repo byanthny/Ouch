12e23217d904abc568c2cc9887d4a7f64d86e5b7
JonoAugustine <jaugust8@jhu.edu>
2019-04-01 12:07:36 -0400

    color fix

--- docs/js/socket.js
+++ docs/js/socket.js
@@ -93,7 +93,7 @@ function play() {
             alert(parsedData.id + "Left");
             document.getElementByClassName(parsedData.id)[0].display = none;
             document.getElementById('chat').innerHTML +=
-                '<p class="chat-msg.enter"><span style="font-weight: bold;">'+
+                '<p class="chat-msg.exit"><span style="font-weight: bold;">'+
                 parsedData.name+' has left the Existence. </span>';
         }
 
