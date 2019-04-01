3bdc6a33729744a18e7da9130cc4c5ffcf22d964
Anthony D <adas22@jhu.edu>
2019-04-01 14:44:41 -0400

    added submit on enter, and shakes when incorrect. Saves username and prompts to retry

--- docs/js/action.js
+++ docs/js/action.js
@@ -1,30 +1,70 @@
-user_input.addEventListener("keydown", function(event) {
+function checkInput(clicked) {
     //not login screen
-    if(!login) {
-        if (event.key === 13 || event.key === "Enter") {
+    if (event.key === 13 || event.key === "Enter" || clicked === true) {
+        var input = user_input.value;
+
+        //not lpgin screen
+        if (!login) {
             event.preventDefault();
-            var input = user_input.value;
             if (input.charAt(0) === "-") {
-                switch(input) {
+                switch (input) {
                     case "-darkmode":
                         switchDark();
                         break;
                     case "-exit":
                         connection.close();
                     default:
-                        console.log("action "+ input);
+                        console.log("action " + input);
                 }
             } else {
                 //console.log("chat");
-                if(connection!=null) {
-                    connection.send('{"dataType":"CHAT","data":"'+input+'"}');
+                if (connection != null) {
+                    connection.send('{"dataType":"CHAT","data":"' + input + '"}');
                 }
             }
             user_input.value = "";
         }
-    }
-    //login screen
-    else  {
 
+        //login screen
+        else {
+            if (input === "") {
+                shakeUsername();
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
+
+user_input.addEventListener("keydown", function (event) {
+    if(checkInput()) {
+        document.getElementById("submit-button").click();
     }
 });
+
+exist_input.addEventListener("keydown", function (event) {
+    if (event.key === 13 || event.key === "Enter") {
+        if(checkInput()) {
+            document.getElementById("submit-button").click();
+        }
+    }
+});
+
+//On button click update connected and stuff
+document.getElementById("submit-button").onclick = function () {
+
+    nickname = document.getElementById('user-input').value;
+    id = document.getElementById('exist-input').value;
+
+    if (checkInput(true)) {
+
+        if (id === "") {
+            url = 'wss://sim-ouch.herokuapp.com/ws?name=' + nickname;
+        } else {
+            url = 'wss://sim-ouch.herokuapp.com/ws?name=' + nickname + '&exID=' + id;
+        }
+
+        play();
+    }
+};
